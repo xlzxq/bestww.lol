2a160b1148000919ec669878822d3e29c2a1a06a
xlzxq <104225234+xlzxq@users.noreply.github.com>
2025-04-30 22:06:40 +0100

    ok ill actually make one now

---
+++ script.js
@@ -0,0 +1,37 @@
+let hasClicked = false;
+
+const links = [
+    { name: "burh", url: "https://www.youtube.com/@Burhy" },
+    { name: "rayloss", url: "https://www.youtube.com/@raylossed" },
+    { name: "xlzxq", url: "https://www.youtube.com/@xlzxq" },
+    { name: "sorr", url: "https://www.youtube.com/@s0rrxo" },
+    { name: "battlerite", url: "https://www.youtube.com/@battleritemc" },
+    { name: "beamerboy", url: "https://www.youtube.com/@beamrboy" },
+    { name: "michaux", url: "https://www.youtube.com/@michauuuux" },
+    { name: "gottya", url: "https://www.youtube.com/@gottya" },
+    { name: "nikazul", url: "https://www.youtube.com/@NiKazul" },
+    { name: "apaxel", url: "https://www.youtube.com/@apaxei" },
+    { name: "cf43", url: "https://www.youtube.com/@cf_43" },
+    { name: "weedy", url: "https://www.youtube.com/@sexyfuckingweedy" }
+];
+
+document.getElementById("title").addEventListener('click', () => {
+    if (!hasClicked) {
+        document.getElementById("title").className = "after-text";
+
+        const container = document.getElementById("image-container");
+        container.classList.add("visible");
+
+        links.forEach(link => {
+            const img = document.createElement("img");
+            img.src = `/assets/images/${link.name}.jpg`;
+            img.alt = link.name;
+            img.addEventListener('click', () => {
+                window.location.href = link.url;
+            });
+            container.appendChild(img);
+        });
+
+        hasClicked = true;
+    }
+});
